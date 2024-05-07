7bee4a427140232187477960c82cd0a2002b601c
XRhahelry <sujalbajracharya1@gmail.com>
2024-05-07 19:29:48 +0545

    prices scraped may 7

--- scraper/price_scraper.py
+++ scraper/price_scraper.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import time
 from datetime import date
@@ -12,8 +14,6 @@ options.add_argument("--disable-application-cache")
 options.add_argument("--disable-cache")
 options.add_argument("--disk-cache-size=0")
 
-# This path works with vscode's code runner plugin.
-# If you get an error try just chromedriver
 service = Service(executable_path="scraper/chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=options)
 
@@ -24,8 +24,6 @@ today = date.today()
 
 for i in range(0, len(links)):
     print(i)
-    # This path works with vscode's code runner plugin.
-    # try ../datasets/prices/ if you are running the file from inside the scraper folder
     filename = "./datasets/prices/" + files[i]
     driver.get(links[i])
     try:
@@ -38,6 +36,14 @@ for i in range(0, len(links)):
                 By.XPATH, '//*[@id="module_product_price_1"]/div/div/span'
             ).text
         except:
+            WebDriverWait(driver, 1).until(
+                EC.presence_of_element_located(
+                    (
+                        By.XPATH,
+                        '//*[@id="module_product_price_1"]/div/div/span',
+                    )
+                )
+            )
             actual_price = driver.find_element(
                 By.XPATH, '//*[@id="module_product_price_1"]/div/div/span'
             ).text
@@ -56,10 +62,10 @@ for i in range(0, len(links)):
         )
     except:
         print(links[i])
-        dd = pd.DataFrame([[links[i], files[i]]], columns=["Failed Url", "Filename"])
+        dd = pd.DataFrame([[files[i], links[i]]], columns=["Filename", "Url"])
         dd.to_csv(
-            "./datasets/failed_prices.csv",
-            header=not os.path.exists("./datasets/failed_prices.csv"),
+            "./datasets/failed.csv",
+            header=not os.path.exists("./datasets/failed.csv"),
             mode="a",
             index=False,
         )
@@ -70,8 +76,6 @@ for i in range(0, len(links)):
 
     df.to_csv(filename, mode="a", header=not os.path.exists(filename), index=False)
     time.sleep(1)
-    if i % 50 == 0:
-        time.sleep(10)
 
 driver.delete_all_cookies()
 driver.quit()
