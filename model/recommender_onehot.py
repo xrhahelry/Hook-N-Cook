0e90de5e0c8985aa79f3a5b51926d0308bf10c15
xrhahelry <sujalbajracharya1@gmail.com>
2024-05-29 16:12:42 +0545

    one hot recommender fixed

--- model/recommender_onehot.py
+++ model/recommender_onehot.py
@@ -7,14 +7,14 @@ enc = df_enc.set_index("id")
 not_enc = df.set_index("id")
 
 choice = "012a16c221"
-vector1 = enc.loc[choice, :].to_numpy()[2:]
+vector1 = enc.loc[choice, :].to_numpy()[3:]
 list1 = not_enc.loc[choice, :].tolist()[:-2]
 
 
 for id, data in enc.iterrows():
     if id == choice:
         continue
-    vector2 = enc.loc[id, :].to_numpy()[2:]
+    vector2 = enc.loc[id, :].to_numpy()[3:]
     # list2 = not_enc.loc[choice, :].tolist()
     distance = np.linalg.norm(vector1 - vector2)
     if distance <= 3:
