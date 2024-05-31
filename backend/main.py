2c926327a9f6be5f9a7a81378286003dcef26f84
xrhahelry <sujalbajracharya1@gmail.com>
2024-05-31 09:43:20 +0545

    user table created

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from config import app, db
+from flask import jsonify, request
+from models import User
+
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    users = User.query.all()
+    json_users = list(map(lambda x: x.to_json(), users))
+    return jsonify({"users": json_users})
+
+
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+
+    if not first_name or not last_name or not email:
+        return jsonify({"message": "Some values misssing"}), 400
+
+    new_user = User(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+
+    return jsonify({"message": "User created!"}), 201
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
