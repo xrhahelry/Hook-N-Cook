24ecfa1c494bbc403992c36e18ce476a08422e8c
xrhahelry <sujalbajracharya1@gmail.com>
2024-06-01 10:30:52 +0545

    user table updated

--- backend/models.py
+++ backend/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     first_name = db.Column(db.String(20), unique=False, nullable=False)
     last_name = db.Column(db.String(20), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    tracked_product = db.Column(db.String(32), unique=True, nullable=True)
 
     def to_json(self):
         return {
