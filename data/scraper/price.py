82c7ab9f480731bdb8cda83adfe84f4303243eec
XRhahelry <sujalbajracharya1@gmail.com>
2024-06-03 21:30:45 +0545

    scraper improved

--- data/scraper/price.py
+++ data/scraper/price.py
@@ -9,12 +9,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def price_scraper(driver):
     df = pd.read_csv("data/scraper/urls.csv")
-    dd = pd.read_csv("data/laptop.csv")
     files = df["Filename"]
     links = df["Url"]
     today = date.today()
 
     for i, link in enumerate(links):
+        data = pd.read_csv("data/laptop.csv")
         if link in [
             "https://www.daraz.com.np/products/dell-precision-3430-sff-core-i7-8700-32ghz-32gb-ram-512gb-solid-state-drive-windows-11-pro-64bit-renewed-i129855105-s1037690646.html?search=1",
             "https://www.daraz.com.np/products/dell-vostro-3888-computer-set-i114454487-s1031087096.html?search=1",
@@ -59,7 +59,8 @@ def price_scraper(driver):
             df["Discount Price"] = (
                 df["Discount Price"].str.replace("Rs. ", "").str.replace(",", "")
             )
-            stock = "yes"
+            data.loc[data.id == files[i].replace(".csv", ""), "instock"] = "yes"
+            data.to_csv("data/laptop.csv", index=False)
         except:
             print(i, filename, link)
             pp = pd.read_csv(filename)
@@ -67,7 +68,7 @@ def price_scraper(driver):
                 [[today, int(pp.iloc[-1, -2]), int(pp.iloc[-1, -1])]],
                 columns=["Date", "Actual Price", "Discount Price"],
             )
-            stock = "no"
+            data.loc[data.id == files[i].replace(".csv", ""), "instock"] = "no"
+            data.to_csv("data/laptop.csv", index=False)
 
         df.to_csv(filename, mode="a", header=not os.path.exists(filename), index=False)
-        dd.loc[dd.id == files[i].replace(".csv", ""), "instock"] = stock
