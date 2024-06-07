4ad16acf3600720fd0e6a5689c61b3468d6cd5e7
xrhahelry <sujalbajracharya1@gmail.com>
2024-06-07 19:56:29 +0545

    auth uncomment

--- hookncook/__init__.py
+++ hookncook/__init__.py
@@ -5,10 +5,10 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "hookncook"
 
-    # from .auth import auth
+    from .auth import auth
     from .views import views
 
     app.register_blueprint(views, url_prefix="/")
-    # app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     return app
