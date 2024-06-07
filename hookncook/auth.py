1fc67fd2e44e884a588b3026842f7dbf388c0f45
YAzz303 <tuladharyajjyu.best@gmail.com>
2024-06-07 14:54:50 +0545

    working on authentication

--- hookncook/auth.py
+++ hookncook/auth.py
@@ -1,11 +1,31 @@
 from flask import Blueprint
+from flask import render_template, request
+from flask_login import login_user, logout_user, current_user, login_required
+
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
-def login():
-    return "<h1>Login</h1>"
+@auth.route("/login/<uid>")
+def login(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("base"))
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "login.html")
+
 
 
 @auth.route("/logout")
@@ -13,6 +33,6 @@ def logout():
     return "<h1>Logout</h1>"
 
 
-@auth.route("/signup")
+@auth.route("/register")
 def signup():
     return "<h1>Sign up</h1>"
---
+++ hookncook/models.py
@@ -0,0 +1,54 @@
+from flask_login import UserMixin
+from config import db
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=False, nullable=False)
+    last_name = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    tracked_product = db.Column(db.String(32), unique=True, nullable=True)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "email": self.email,
+        }
+
+
+class Laptop(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    brand = db.Column(db.String, unique=False, nullable=False)
+    model = db.Column(db.String, unique=False, nullable=False)
+    processor = db.Column(db.Integer, unique=False, nullable=False)
+    ram_memory = db.Column(db.Integer, unique=False, nullable=False)
+    display_size = db.Column(db.Float, unique=False, nullable=False)
+    storage_capacity = db.Column(db.Integer, unique=False, nullable=False)
+    cpu_cores = db.Column(db.Integer, unique=False, nullable=False)
+    graphics_card = db.Column(db.Float, unique=False, nullable=False)
+    rating = db.Column(db.Float, unique=False, nullable=False)
+    reviews = db.Column(db.Integer, unique=False, nullable=False)
+    name = db.Column(db.String, unique=False, nullable=False)
+    url = db.Column(db.String, unique=True, nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "price": self.price,
+            "brand": self.brand,
+            "model": self.model,
+            "processor": self.processor,
+            "ramMemory": self.ram_memory,
+            "displaySize": self.display_size,
+            "storageCapacity": self.storage_capacity,
+            "cpuCores": self.cpu_cores,
+            "graphicsCard": self.graphics_card,
+            "rating": self.rating,
+            "reviews": self.reviews,
+            "name": self.name,
+            "url": self.url,
+        }
