7def03c2926eac43ca3a5f13975ed80580a43f8b
XRhahelry <sujalbajracharya1@gmail.com>
2024-06-14 22:14:26 +0545

    seperated files into  better folder data cleaning and analysis in singular seperate file

---
+++ scraper/scraper.py
@@ -0,0 +1,27 @@
+import price
+import specs
+import url
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+options.add_argument("--disable-application-cache")
+options.add_argument("--disable-cache")
+options.add_argument("--disk-cache-size=0")
+
+service = Service(executable_path="scraper/chromedriver.exe")
+driver = webdriver.Chrome(service=service, options=options)
+
+
+def main():
+    url.url_scraper(driver)
+    price.price_scraper(driver)
+    specs.specs_scraper(driver)
+
+
+if __name__ == "__main__":
+    main()
+
+driver.delete_all_cookies()
+driver.quit()
---
+++ scraper/url.py
@@ -0,0 +1,28 @@
+import hashlib
+
+import pandas as pd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def url_scraper(driver):
+    links = []
+    for i in range(0, 42):
+        url = f"https://www.daraz.com.np/laptops/?page={i}"
+        driver.get(url)
+        products = driver.find_elements(By.XPATH, '//*[@id="id-a-link"]')
+        urls = [x.get_property("href") for x in products]
+        links += urls
+
+    def generate_filename(url):
+        hashed_url = hashlib.sha256(url.encode()).hexdigest()
+        truncated_url = hashed_url[:10]
+        return truncated_url
+
+    files = [generate_filename(url) for url in links]
+    files = [file + ".csv" for file in files]
+
+    df = pd.DataFrame(list(zip(links, files)), columns=["Url", "Filename"])
+    df.drop_duplicates(inplace=True)
+    df.to_csv("data/urls.csv", index=False)
